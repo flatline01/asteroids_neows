73a934e81c5eb1e6044132c5d9602d3c2f2a86c4
Joe Porto <cthulhu0@gmail.com>
2021-10-03 11:59:48 -0400

    set up build and serve from root.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('/public/dist/index.html', { root: __dirname });
+  res.sendFile('/build/index.html', { root: __dirname });
 });
 
 router.get("/testroute", function(req,res,next) {
